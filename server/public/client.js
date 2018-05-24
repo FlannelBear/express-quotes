54fd495973a61367701fea95570a9d700db46737
Flannel Bear <josh.flannelbear@gmail.com>
2018-05-23 22:47:09 -0500

    finished base mode

---
+++ server/public/client.js
@@ -0,0 +1,42 @@
+// EXPRESS QUOTES
+$(document).ready(() => {
+    $('#refreshButton').on('click', getQuotes);
+    $('#addQuoteButton').on('click', addQuote);
+});
+
+
+function getQuotes(){
+    console.log('in getQuotes');
+    $.ajax({
+        method: 'GET',
+        url: '/quotes'
+    }).then(function(response){
+        console.log('in .then of getQuotes');
+        appendQuotes(response);
+        });
+}
+
+function appendQuotes(array){
+    console.log('in appendQuotes');
+    $('#appendQuotesHere').empty();
+    for( let i = 0; i < array.length; i++ ){
+        console.log('in for loop');
+        $('#appendQuotesHere').append(`<div><p>${array[i].text}<p><p>by: ${array[i].author}.<p><div>`);
+    }
+}
+
+function addQuote(){
+    console.log('in addQuote');
+    let text = $('#quoteText').val();
+    let author = $('#quoteAuthor').val();
+
+    let quoteObject = {text: `${text}`, author: `${author}`};
+
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        data: quoteObject
+    }).then(function(response){
+        appendQuotes(response);
+    });
+}
--- server/server.js
+++ server/server.js
@@ -4,9 +4,23 @@ const bodyParser = require('body-parser');
 // globals
 const app = express();
 const port = 5000;
+
+let quotes = [{
+    text: '"You miss 100% of the shots you dont take - Wayne Gretzky"',
+    author: 'Michael Scott'
+},
+{
+    text: '"Which bear is best? False. The black bear."',
+    author: 'Dwight Schrute'
+},
+{
+    text: '"Things arent very nice in the clink."',
+    author: `Prison Mike`
+}
+];
 // uses
 app.use( express.static('server/public'));
-
+app.use( bodyParser.urlencoded({extended: true}));
 // spin up server
 app.listen(port, () => {
     console.log('server running on port:', port);
@@ -14,4 +28,15 @@ app.listen(port, () => {
 
 // get route
 
+app.get( '/quotes', (req, res) => {
+    console.log('get hit for /quotes route');
+    res.send(quotes);
+});
+
 // post route
+
+app.post('/quotes', (req, res) => {
+    console.log('post hit for /quotes route');
+    quotes.push(quoteObject);
+    res.send(quotes);
+});
